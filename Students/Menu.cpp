3f2abfdcd378a326bca28a122e6dc578adcc1cf1
VD <dominick.odessa@gmail.com>
2023-10-09 23:14:36 +0300

    Adding last funks to Menu

--- Students/Menu.cpp
+++ Students/Menu.cpp
@@ -45,3 +45,40 @@ void MenuRemoveStudent(ArrayStudents& a)//////////////////   DZ
 	else cout << "Student not found. No data to delete." << endl;
 	_getch();
 }
+
+void MenuEditStudent(ArrayStudents& a)
+{
+	system("cls");
+
+	Student st[15];
+	cout << "Введите имя студента: ";
+	cin >> st->Name;
+	cout << "Введите фамилию студента: ";
+	cin >> st->Surname;
+
+
+	int index = FindStudentIndex(a, st);
+	if (index >= 0)
+		EditStudent(a, index);
+	else cout << "Student not found" << endl;
+	_getch();
+}
+
+void MenuPrintStudents(const ArrayStudents& a)
+{
+	system("cls");
+
+	cout << setw(15);
+	cout << "Имя";
+	cout << setw(15);
+	cout << "Фамилия";
+	cout << setw(10);
+	cout << "Возраст";
+	cout << setw(20);
+	cout << "Телефон";
+	cout << setw(15);
+	cout << "Средний балл";
+	cout << endl << endl;
+	PrintStudents(a);
+	_getch();
+}

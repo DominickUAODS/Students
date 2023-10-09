9b598300f8214d0deb88f792a532d92c5d0edb09
VD <dominick.odessa@gmail.com>
2023-10-09 23:09:23 +0300

    Add structure Student

---
+++ Students/Student.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+struct Student
+{
+	char Name[20];/////////////////////20
+	char Surname[20];////////////////20
+	int Age;/////////////////////////4
+	char Phone[20];////////////////20
+	double GPA; // grade point average   8
+};

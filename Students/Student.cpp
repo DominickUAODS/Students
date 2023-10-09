f7946db5f294aaca6836e00bc3dbbf0590490492
VD <dominick.odessa@gmail.com>
2023-10-09 23:16:36 +0300

    void AddStudents(ArrayStudents& a, Student* st)

--- Students/Student.cpp
+++ Students/Student.cpp
@@ -13,3 +13,16 @@ void InputData(Student* st)////вже Ї студент в пам'€т≥
 	cout << "¬ведите средний балл студента: ";
 	cin >> st->GPA;
 }
+
+void AddStudents(ArrayStudents& a, Student* st)
+{
+	int new_num = a.Count + 1;
+
+	Student** temp = new Student * [new_num];
+	for (int i = 0; i < a.Count; i++)
+		temp[i] = a.list[i];
+
+	delete[]a.list;
+	a.list = temp;
+	a.list[a.Count++] = st;
+}
